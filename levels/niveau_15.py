56b1d4de6b307ab820973661d5f126b0cde0a845
pythonfan972 <87706832+PLL-process@users.noreply.github.com>
2025-05-06 12:06:29 -0400

    Add files via upload

---
+++ levels/niveau_15.py
@@ -0,0 +1,14 @@
+"""
+Niveau 15 – PyChuckie
+Objectif : compléter le script pour résoudre le niveau.
+
+Compétences visées : Boucles, Conditions, Fonctions, Structures de données (selon le niveau)
+
+"""
+
+def jouer_niveau():
+    print("Bienvenue au niveau 15 !")
+    # 🧩 Code à compléter par l'élève
+    pass
+
+jouer_niveau()
---
+++ levels/niveau_7.py
@@ -0,0 +1,14 @@
+"""
+Niveau 7 – PyChuckie
+Objectif : compléter le script pour résoudre le niveau.
+
+Compétences visées : Boucles, Conditions, Fonctions, Structures de données (selon le niveau)
+
+"""
+
+def jouer_niveau():
+    print("Bienvenue au niveau 7 !")
+    # 🧩 Code à compléter par l'élève
+    pass
+
+jouer_niveau()
